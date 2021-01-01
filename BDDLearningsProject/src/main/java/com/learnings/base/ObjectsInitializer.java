06b4c6f4ed691d27e9c5176a9ac1b0361d615698
Naga <Naga@DESKTOP-N029N8S.lan>
2021-01-01 10:11:03 -0800

    Update

--- BDDLearningsProject/src/main/java/com/learnings/base/ObjectsInitializer.java
+++ BDDLearningsProject/src/main/java/com/learnings/base/ObjectsInitializer.java
@@ -30,10 +30,11 @@ public class ObjectsInitializer {
 				
 				}else {
 					chromeOptions.addArguments("--headless");
-					chromeOptions.addArguments("--nosandbox");
-					chromeOptions.addArguments("--disable-dev-shm-usage");
-					chromeOptions.addArguments("-disable-browser-side-navigation");
+					
 				}
+			chromeOptions.addArguments("--nosandbox");
+			chromeOptions.addArguments("--disable-dev-shm-usage");
+			chromeOptions.addArguments("-disable-browser-side-navigation");
 			driver.set(new ChromeDriver(chromeOptions));
 			
 		}else if(browserName.contains("Firefox")) {
